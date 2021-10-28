4a9f63a80e84862d65a062d3d1787cb0cd73d070
Mya Gwinn <mvgwinn1121@eagle.fgcu.edu>
2021-10-28 19:05:11 -0400

    updated all

---
+++ Client/Animal.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1
+{
+    public class Animal
+    {
+        public static string correctAnimal = "Parrot!";
+    }
+}
